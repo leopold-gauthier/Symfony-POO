c699c78d1a0b816ce4be3ed8242aa3fe49343176
leopold-gauthier <leopold.gauthier-de-porry@laplateforme.io>
2024-01-11 11:53:00 +0100

    add form for add product

--- src/Controller/ProduitsController.php
+++ src/Controller/ProduitsController.php
@@ -2,17 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
+use App\Form\AjoutProduitType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProduitsController extends AbstractController
 {
-    #[Route('/produits', name: 'app_produits')]
-    public function index(): Response
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        return $this->render('produits/index.html.twig', [
-            'controller_name' => 'ProduitsController',
-        ]);
+        $this->entityManager = $entityManager;
+    }
+
+
+    #[Route('/produits', name: 'app_produits')]
+    public function new(Request $request): Response
+    {
+        $produit = new Produit();
+        $form = $this->createForm(AjoutProduitType::class, $produit, ['attr' => ["class" => "test"]]);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            $produit = $form->getData();
+
+            $this->entityManager->persist($produit);
+            $this->entityManager->flush();
+            return $this->redirect($request->getUri());
+        }
+
+        return $this->render(
+            "produits/index.html.twig",
+            ['formProduit' => $form->createView()]
+        );
     }
 }
---
+++ src/Form/AjoutProduitType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Produit;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+class AjoutProduitType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class,[
+                'attr' => ['class' => 'formulaire']
+            ])
+            ->add('stock')
+            ->add('description')
+            ->add('categories', EntityType::class, [
+                'required' => false,
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+            ])
+            ->add('submit',SubmitType::class,[
+                'label' => 'Ajouter Produit'
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Produit::class,
+        ]);
+    }
+}
